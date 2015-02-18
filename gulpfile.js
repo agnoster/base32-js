ae67b68454dc1106f5991a7add682b46c40d1aa0
Michael Cole <michaelcole@michaelcole.com>
2015-02-18 10:23:53 +0700

    Simplified gulpfile.js, but still failing on 0.8

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,17 @@
 'use strict';
 
 var gulp = require('gulp');
-var del = require('del');
-var plugins = require('gulp-load-plugins')();
-
-
-// Add a task to render the output 
-
-gulp.task('help', plugins.taskListing);
- 
-
-// Clean
-
-gulp.task('clean', function (cb) {
-  del('dist', cb);
-});
-
+var gulp_uglify = require('gulp-uglify');
+var gulp_rename = require('gulp-rename');
 
 // Build
 
-gulp.task('build', ['clean'], function (cb) {
+gulp.task('default', function (cb) {
   return gulp.src('lib/base32.js')
   // copy original to dist
   .pipe(gulp.dest('dist'))
   // uglify and copy to dist
-  .pipe(plugins.uglify())
-  .pipe(plugins.rename('base32.min.js'))
+  .pipe(gulp_uglify())
+  .pipe(gulp_rename('base32.min.js'))
   .pipe(gulp.dest('dist'));
 });
-
-
-// Default is to build
-
-gulp.task('default', ['build']);
